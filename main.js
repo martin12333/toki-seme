48fc81a18d7a0ef77193e8685611a5728ec554d5
Dustin Dikes <dustindikes@gmail.com>
2015-07-30 11:52:39 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,204 @@
+var nomi = {
+    "a" : "interj ah, ha, uh, oh, ooh, aw, well (emotion word)",
+    "akesi" : "n non-cute animal, reptile, amphibian",
+    "ala" : "mod no, not, none, un-\nn nothing, negation, zero\ninterj no!",
+    "alasa" : "vt gather, hunt",
+    "ale" : "n everything, anything, life, the universe\nmod all, every, complete, whole",
+    "ali" : "n everything, anything, life, the universe\nmod all, every, complete, whole",
+    "anpa" : "n bottom, lower part, under, below, floor, beneath\nmod low, lower, bottom, down",
+    "ante" : "n difference\nmod different\nconj otherwise, or else\nvt change, alter, modify",
+    "anu" : "conj or",
+    "awen" : "vi stay, wait, remain\nvt keep\nmod remaining, stationary, permanent, sedentary",
+    "e" : "sep (introduces a direct object)",
+    "en" : "conj and (used to coordinate head nouns)",
+    "esun" : "n market, shop",
+    "ijo" : "n thing, something, stuff, anything, object\nmod of something\nvt objectify",
+    "ike" : "mod bad, negative, wrong, evil, overly complex, (figuratively) unhealthy\ninterj oh dear! woe! alas!\nn negativity, badness, evil\nvt to make bad, to worsen, to have a negative effect upon\nvi to be bad, to suck\n[sounds like icky]",
+    "ilo" : "n tool, device, machine, thing used for a specific purpose",
+    "insa" : "n inside, inner world, centre, stomach\nmod inner, internal\n[inside]",
+    "jaki" : "mod dirty, gross, filthy\nn dirt, pollution, garbage, filth\nvt pollute, dirty\ninterj ew! yuck!\n[yucky]",
+    "jan" : "n person, people, human, being, somebody, anybody\nmod human, somebody's, personal, of people\nvt personify, humanize, personalize",
+    "jelo" : "mod yellow, light green\n[yellow]",
+    "jo" : "vt have, contain\nn having\nkama receive, get, take, obtain",
+    "kala" : "n fish, sea creature",
+    "kalama" : "n sound, noise, voice\nvi make noise\nvt sound, ring, play (an instrument)",
+    "kama" : "vi come, become, arrive, happen, pursue actions to arrive to (a certain state), manage to, start to\nn event, happening, chance, arrival, beginning\nmod coming, future\nvt bring about, summon\n[come up]",
+    "kasi" : "n plant, leaf, herb, tree, wood",
+    "ken" : "vi can, is able to, is allowed to, may, is possible\nn possibility, ability, power to do things, permission\nvt make possible, enable, allow, permit\ncont it is possible that",
+    "kepeken" : "vt use\nprep with",
+    "kili" : "n fruit, pulpy vegetable, mushroom",
+    "kin" : "mod also, too, even, indeed (emphasizes the word(s) before it)",
+    "kipisi" : "(yet undefined entry in the official word list)",
+    "kiwen" : "mod hard, solid, stone-like, made of stone or metal\nn hard thing, rock, stone, metal, mineral, clay",
+    "ko" : "n semi-solid or squishy substance, e.g. paste, powder, gum",
+    "kon" : "n air, wind, smell, soul\nmod air-like, ethereal, gaseous",
+    "kule" : "n colour, paint\nmod colourful\nvt colour, paint",
+    "kulupu" : "n group, community, society, company, people\nmod communal, shared, public, of the society",
+    "kute" : "vt listen, hear\nmod auditory, hearing",
+    "la" : "sep (between adverb or phrase of context and sentence)",
+    "lape" : "n, vi sleep, rest\nmod sleeping, of sleep",
+    "laso" : "mod blue, blue-green",
+    "lawa" : "n head, mind\nmod main, leading, in charge\nvt lead, control, rule, steer",
+    "len" : "n clothing, cloth, fabric",
+    "lete" : "n cold\nmod cold, uncooked\nvt cool down, chill",
+    "li" : "sep (between any subject except mi and sina and its verb; also used to introduce a new verb for the same subject)",
+    "lili" : "mod small, little, young, a bit, short, few, less\nvt reduce, shorten, shrink, lessen",
+    "linja" : "n long, very thin, floppy thing, e.g. string, rope, hair, thread, cord, chain",
+    "lipu" : "n flat and bendable thing, e.g. paper, card, ticket",
+    "loje" : "mod red",
+    "lon" : "prep be (located) in/at/on\nvi be there, be present, be real/true, exist, be awake",
+    "luka" : "n hand, arm",
+    "lukin" : "vt see, look at, watch, read\nvi look, watch out, pay attention\nmod visual(ly)\n[looking]",
+    "lupa" : "n hole, orifice, window, door\n[sounds like loop]",
+    "ma" : "n land, earth, country, (outdoor) area",
+    "mama" : "n parent, mother, father\nmod of the parent, parental, maternal, fatherly\n[sounds like momma]",
+    "mani" : "n money, material wealth, currency, dollar, capital\n[money]",
+    "meli" : "n woman, female, girl, wife, girlfriend\nmod female, feminine, womanly\n[Mary]",
+    "mi" : "n I, we\nmod my, our\n[me]",
+    "mije" : "n man, male, boy, husband, boyfriend\nmod male, masculine, manly",
+    "moku" : "n food, meal\nvt eat, drink, swallow, ingest, consume",
+    "moli" : "n death\nvi die, be dead\nvt kill\nmod dead, deadly, fatal",
+    "monsi" : "n back, rear end, butt, behind\nmod back, rear",
+    "mu" : "interj woof! meow! moo! etc. (cute animal noise)\n[moo]",
+    "mun" : "n moon\nmod lunar\n[moon]",
+    "musi" : "n fun, playing, game, recreation, art, entertainment\nmod artful, fun, recreational\nvi play, have fun\nvt amuse, entertain",
+    "mute" : "mod many, very, much, several, a lot, abundant, numerous, more\nn amount, quantity\nvt make many or much\n[multi]",
+    "namako" : "n food additive, accessory, something extra\nvt season, embellish, stimulate",
+    "nanpa" : "n number\noth -th (ordinal numbers)\n[number]",
+    "nasa" : "mod silly, crazy, foolish, drunk, strange, stupid, weird\nvt drive crazy, make weird",
+    "nasin" : "n way, manner, custom, road, path, doctrine, system, method",
+    "nena" : "n bump, nose, hill, mountain, button",
+    "ni" : "mod this, that",
+    "nimi" : "n word, name",
+    "noka" : "n leg, foot",
+    "o" : "sep O (vocative or imperative)\ninterj hey! (calling somebody's attention)",
+    "oko" : "n eye\n[similar to oculist]",
+    "olin" : "n love\nmod love\nvt to love (a person)",
+    "ona" : "n she, he, it, they\nmod her, his, its, their",
+    "open" : "vt open, turn on",
+    "pakala" : "n blunder, accident, mistake, destruction, damage, breaking\nvt screw up, fuck up, botch, ruin, break, hurt, injure, damage, spoil, ruin\nvi screw up, fall apart, break\ninterj damn! fuck!",
+    "pali" : "n activity, work, deed, project\nmod active, work-related, operating, working\nvt do, make, build, create\nvi act, work, function",
+    "palisa" : "n long, mostly hard object, e.g. rod, stick, branch",
+    "pan" : "n grain, cereal",
+    "pana" : "vt give, put, send, place, release, emit, cause\nn giving, transfer, exchange",
+    "pi" : "sep of, belonging to",
+    "pilin" : "n feelings, emotion, heart\nvi feel\nvt feel, think, sense, touch\n[feeling]",
+    "pimeja" : "mod black, dark\nn darkness, shadows\nvt darken",
+    "pini" : "n end, tip\nmod completed, finished, past, done, ago\nvt finish, close, end, turn off",
+    "pipi" : "n bug, insect, spider",
+    "poka" : "n side, hip, next to\nprep in the accompaniment of, with\nmod neighbouring",
+    "poki" : "n container, box, bowl, cup, glass\n[box]",
+    "pona" : "n good, simplicity, positivity\nmod good, simple, positive, nice, correct, right\ninterj great! good! thanks! OK! cool! yay!\nvt improve, fix, repair, make good\n[bonam]",
+    "pu" : "(yet undefined entry in the official word list)",
+    "sama" : "mod same, similar, equal, of equal status or position\nprep like, as, seem",
+    "seli" : "n fire, warmth, heat\nmod hot, warm, cooked\nvt heat, warm up, cook",
+    "selo" : "n outside, surface, skin, shell, bark, shape, peel",
+    "seme" : "oth what, which, wh- (question word)",
+    "sewi" : "n high, up, above, top, over, on\nmod superior, elevated, religious, formal",
+    "sijelo" : "n body, physical state",
+    "sike" : "n circle, wheel, sphere, ball, cycle\nmod round, cyclical",
+    "sin" : "mod new, fresh, another, more\nvt renew, renovate, freshen",
+    "sina" : "n you\nmod your",
+    "sinpin" : "n front, chest, torso, face, wall",
+    "sitelen" : "n picture, image\nvt draw, write",
+    "sona" : "n knowledge, wisdom, intelligence, understanding\nvt know, understand, know how to\nvi know, understand\nkama learn, study",
+    "soweli" : "n animal, especially land mammal, lovable animal",
+    "suli" : "mod big, tall, long, adult, important\nvt enlarge, lengthen\nn size",
+    "suno" : "n sun, light",
+    "supa" : "n horizontal surface, e.g furniture, table, chair, pillow, floor",
+    "suwi" : "n candy, sweet food\nmod sweet, cute \nvt sweeten\n[sweet]",
+    "tan" : "prep from, by, because of, since\nn origin, cause",
+    "taso" : "mod only, sole\nconj but\n[that's all]",
+    "tawa" : "prep to, in order to, towards, for, until\nvi go to, walk, travel, move, leave\nn movement, transportation\nmod moving, mobile\nvt move, displace\n[towards]",
+    "telo" : "n water, liquid, juice, sauce\nvt water, wash with water",
+    "tenpo" : "n time, period of time, moment, duration, situation",
+    "toki" : "n language, talking, speech, communication\nmod talking, verbal\nvt say\nvi talk, chat, communicate\ninterj hello! hi!",
+    "tomo" : "n indoor constructed space, e.g. house, home, room, building\nmod urban, domestic, household",
+    "tu" : "mod two\nn duo, pair\nvt double, separate/cut/divide in two\n[two]",
+    "unpa" : "n sex, sexuality\nmod erotic, sexual\nvt have sex with, sleep with, fuck\nvi have sex",
+    "uta" : "n mouth\nmod oral",
+    "utala" : "n conflict, disharmony, competition, fight, war, battle, attack, blow, argument, physical or verbal violence\nvt hit, strike, attack, compete against",
+    "walo" : "mod white, light (colour)\nn white thing or part, whiteness, lightness\n[sounds like wall, which is often white]",
+    "wan" : "mod one, a\nn unit, element, particle, part, piece\nvt unite, make one\n[one]",
+    "waso" : "n bird, winged animal",
+    "wawa" : "n energy, strength, power\nmod energetic, strong, fierce, intense, sure, confident\nvt strengthen, energize, empower",
+    "weka" : "mod away, absent, missing\nn absence\nvt throw away, remove, get rid of",
+    "wile" : "vt to want, need, wish, have to, must, will, should\nn desire, need, will\nmod necessary"
+};
+
+$('a.btn-success').on('click',function(e){
+    e.preventDefault();
+    processWords();
+});
+
+$(document).ready(function(){
+    var search = window.location.search;
+    if( search.indexOf('?t=') !== -1 )
+    {
+        var words = search.replace('\?t=','').replace(/\\n/g,"\n").split('%20');
+        $('textarea').val(search.replace('\?t=','').replace(/\\n/g,"\n").replace(/%20/g,' '));
+        processWords(words);
+    }
+
+    var dict_output = '';
+    for( x in nomi )
+    {
+        dict_output += '<dt>'+x+'</dt><dd>'+nomi[x].replace(/\n/g,'<br>')+'</dd>'
+    }
+    $('#dict .well dl').html(dict_output);
+});
+
+function processWords(words)
+{
+    if( words == undefined )
+    {
+        var sentence = $('textarea').val();
+        var words = sentence.split(" ");
+    }
+    var output = '';
+    var url = window.location.origin+'/toki-seme/?t=';
+
+    for( x in words )
+    {
+        if( words[x] != '' )
+        {
+            if( words[x].indexOf("\n") !== -1 )
+            {
+                url += (x!=0?'%20':'')+words[x].replace(/ /g,'%20').replace('\n','\\n');
+                output += translateWords(words[x].split("\n"),true);
+            }
+            else
+            {
+                url += (x!=0?'%20':'')+words[x];
+                output += translateWords([words[x]],false);
+            }
+        }
+    }
+
+    output += '<br><br><p>URL to this translation: <a href="'+url+'">'+url+'</a></p>';
+
+    window.history.replaceState( {} , '', url );
+
+    $('.output').html(output);
+    $('[data-toggle="tooltip"]').tooltip();
+}
+
+function translateWords(words,breaks)
+{
+    var output = '';
+    for( x in words )
+    {
+        if( words[x] != "" )
+        {
+            var key = words[x].toLowerCase().replace(/[^A-Za-z]+/g, '');
+
+            if( nomi[key] != undefined )
+                output += '<button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="bottom" title="'+nomi[key]+'">'+words[x]+'</button>&nbsp;';
+            else
+                output += '<button type="button" class="btn btn-danger" data-toggle="tooltip" data-placement="bottom" title="Could not find match">'+words[x]+'</button>&nbsp;';
+        }
+        if( breaks && x != words.length-1 )
+            output += '<br>';
+    }
+    return output;
+}
